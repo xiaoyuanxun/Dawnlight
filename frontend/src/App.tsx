2ef31e64f2e9c79675c190039600067a3e99fc8e
Shuaige1234567 <dujunhongshishuaige@gmail.com>
2023-12-21 18:53:26 +0800

    主页

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,17 @@ import {Header} from "./components/header";
 import {Content} from "./components/content";
 import {UserPage} from "./components/user";
 import {Explore} from "./components/explore";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Explore/>
+      <Routes>
+        <Route path={"/"} element={<Content/>}/>
+        <Route path={"explore"} element={<Explore/>}/>
+        <Route path={"address/:address"} element={<UserPage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/assetPanel/index.tsx
@@ -0,0 +1,61 @@
+import React, {useState} from "react";
+import styles from "./index.less"
+
+const Trades = React.memo(() => {
+  return <>
+    {new Array(100).fill(0).map((v, k) => {
+      return <div className={styles.panel_item} key={k}>
+        <span>🛁</span>
+        <span>0x8d</span>
+        <span className={styles.panel_item_tag}>SOLD</span>
+        <span> 0.03 share for 0.03600 ETH</span>
+      </div>
+    })}
+  </>
+})
+
+const Holders = React.memo(() => {
+  return <>
+    <div className={styles.holder_header}>
+      <span>HOLDER</span>
+      <span>SHARE</span>
+    </div>
+    <div className={styles.holder_item}>
+      <span>
+        🌱 0xD10b...068D
+      </span>
+      <span>6</span>
+    </div>
+  </>
+})
+
+const Overview = React.memo(() => {
+  return <>
+    <div style={{marginBottom:"24px"}}>
+      <div className={styles.overview_title}>Total Value in the Pool</div>
+      <div className={styles.overview_content}>$124093.33</div>
+      <div className={styles.overview_des}>56.54393 ETH</div>
+    </div>
+    <div>
+      <div className={styles.overview_title}>Share Supply</div>
+      <div className={styles.overview_content}>142.400844000000000001 Shares</div>
+    </div>
+  </>
+})
+
+export const AssetPanel = React.memo(() => {
+  const [tag, setTag] = useState(0)
+  const arr = ["Recent Trades", "Holders", "Overview"]
+
+  return <div className={styles.panel_wrap}>
+    <div className={styles.panel_header}>
+      {arr.map((v, k) => {
+        return <span onClick={() => setTag(k)} key={k} className={tag === k ? styles.click : styles.default}>{v}</span>
+      })}
+    </div>
+    <div className={styles.panel_main}>
+      {tag === 0 ? <Trades/> : tag === 1 ? <Holders/> : <Overview/>}
+    </div>
+  </div>
+})
+
